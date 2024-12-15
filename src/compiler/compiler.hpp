cb6b207994d7d3d71a7f5b5286169a6adae0dec8
Brody Cooper <reed.thorngag@gmail.com>
2024-12-15 21:34:15 +1300

    starting on float support

--- src/compiler/compiler.hpp
+++ src/compiler/compiler.hpp
@@ -37,7 +37,7 @@ namespace Compiler {
         Size::DWORD,// u32,
         Size::QWORD,// u64,
 
-        Size::WORD,// f16, these will require special handling prob (in xmm regs?)
+        Size::WORD,// f16, these will require special handling prob
         Size::DWORD,// f32,
         Size::QWORD,// f64,
 
@@ -67,7 +67,23 @@ namespace Compiler {
         RDI,
         RBP,
         RSP,
-        STACK // simplifies stuff for what reg params use
+        STACK, // simplifies stuff for what reg params use
+        XMM0,
+        XMM1,
+        XMM2,
+        XMM3,
+        XMM4,
+        XMM5,
+        XMM6,
+        XMM7,
+        XMM8,
+        XMM9,
+        XMM10,
+        XMM11,
+        XMM12,
+        XMM13,
+        XMM14,
+        XMM15
     };
 
     struct Local {
